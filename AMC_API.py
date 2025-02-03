b17bb226f0080d5ea91237d129c759aab47a9b71
Mark Si <mark.si@emory.edu>
2025-02-02 23:56:25 -0800

    1

--- AMC_API.py
+++ AMC_API.py
@@ -4,7 +4,7 @@ import serpapi
 import os
 
 def google_request(amc_name):
-    api_key = os.getenv("GOOG_API_KEY")
+    api_key = os.getenv("API_KEY")
     print("API Key is set" if api_key else "API Key not found")
     params = {
         "q": amc_name,
